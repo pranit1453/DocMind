18f69a28f7025ee6472b5b1243495c7d555c1436
Pranit Bhangale <pranitbhangale1@gmail.com>
2026-08-23 16:00:28 +0530

    Added: Swagger Configuration

---
+++ docmind-backend/doc-mind/src/main/java/com/pranit/docmind/swagger/SwaggerConfig.java
@@ -0,0 +1,33 @@
+package com.pranit.docmind.swagger;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "DocMind API",
+                description = "Document-based RAG system API",
+                version = "1.0.0",
+                summary = "DocMind API",
+                contact = @Contact(
+                        name = "Pranit Bhangale",
+                        url = "https://pranitbhangale.vercel.app",
+                        email = "pranitbhangale1453@gmail.com"
+                )
+        )
+)
+@SecurityScheme(
+        name = "accessToken",
+        type = SecuritySchemeType.APIKEY,
+        in = SecuritySchemeIn.COOKIE,
+        paramName = "access_token",
+        description = "JWT access token stored in HttpOnly cookie"
+)
+public final class SwaggerConfig {
+}
